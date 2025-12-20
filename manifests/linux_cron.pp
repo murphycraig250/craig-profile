7936620e05d7f6222bca1a23b7ae8b9bce40e82e
craig <murphycraig250@gmai.com>
2025-12-20 21:05:37 +0000

    update cron minute

--- manifests/linux_cron.pp
+++ manifests/linux_cron.pp
@@ -2,7 +2,7 @@ class profile::linux_cron {
   include cron
 
   cron::job { 'datetemp':
-    minute  => '5',
+    minute  => '*/5',
     user    => 'root',
     command => '/bin/date >> /tmp/cron',
   }
