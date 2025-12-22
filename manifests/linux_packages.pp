4da3942aaa93872bdefdef3fa229ac5b253ac3da
craig <murphycraig250@gmai.com>
2025-12-22 11:28:15 +0000

    add linux package profile

---
+++ manifests/linux_packages.pp
@@ -0,0 +1,19 @@
+class profile linux_packages {
+
+$default_packges = [
+  'cmatrix',
+]
+
+$extra_packages   = lookup('packages::install', { default_value => [] })
+$remove_packages  = lookup('packages::remove',  { default_value => [] })
+
+$final_packages = ($default_packages + $extra_packages).unique - $remove_packages
+
+  package { $final_packages:
+    ensure => installed,
+  }
+
+  package { $remove_packages:
+    ensure => absent,
+  }
+}
