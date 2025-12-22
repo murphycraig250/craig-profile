652bac9b7a4badfebc93eff00442654e3ae7d25b
craig <murphycraig250@gmai.com>
2025-12-22 14:14:11 +0000

    updated users profile

--- manifests/linux_user.pp
+++ manifests/linux_user.pp
@@ -2,8 +2,10 @@ class profile::linux_user {
     include accounts 
         accounts::user { 'bradley': }
         accounts::user { 'lucas': 
-            password => 'lucas',
+            password => '$6$gfPtUzuaQGcWC/fJ$96SzKL41OBUi8P8ckM.Nx8G5or2nMQP1VcGOQ7csw1gd4jtNIWyZar6/YcyyvJSy6NIvyrSjGnZK74qT6AZQc0',
             locked   => false,
+            groups   => 'users',
+            shell    => '/bin/bash',
         }
         accounts::user { 'theo': }
     }
