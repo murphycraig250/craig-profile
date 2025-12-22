60b8a7c5dfbdfbca22304f0baf8ac4bd0589d488
craig <murphycraig250@gmai.com>
2025-12-22 20:09:03 +0000

    add nfs client

---
+++ manifests/linux_nfsclient.pp
@@ -0,0 +1,22 @@
+class profile::linux_nfsclient {
+
+  package { 'nfs-common':
+    ensure => installed,
+  }
+
+  file { '/mnt/network_data':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'labadmins',
+    mode   => '0775',
+  }
+
+  mount { '/mnt/network_data':
+    ensure  => 'mounted',
+    device  => 'ubuntu-primary:/srv/nfs_share', 
+    fstype  => 'nfs',
+    options => 'defaults,_netdev',
+    atboot  => true,
+    require => [Package['nfs-common'], File['/mnt/network_data']],
+  }
+}
