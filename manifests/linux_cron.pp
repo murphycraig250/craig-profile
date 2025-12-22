b1729045d1e1387316c01f90ea0ab12cf4c6dbaf
craig <murphycraig250@gmai.com>
2025-12-22 16:17:58 +0000

    added cron job

--- manifests/linux_cron.pp
+++ manifests/linux_cron.pp
@@ -6,4 +6,12 @@ class profile::linux_cron {
     user    => 'root',
     command => '/bin/date >> /tmp/cron',
   }
+  
+  $cron_jobs = lookup('cron_jobs::create', Hash, 'deep', {} )
+
+  $cron_jobs.each | String $job_name, Hash $attributes| {
+    cron::job { $job_name:
+    user => 'root',
+    *    =>  $attributes,
+    }
 }
