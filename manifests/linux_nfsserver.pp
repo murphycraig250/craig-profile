e83b96979dfb90348539fbad9bd71de32cd019f1
craig <murphycraig250@gmai.com>
2025-12-22 19:30:34 +0000

    add nfsserver profile

---
+++ manifests/linux_nfsserver.pp
@@ -0,0 +1,25 @@
+class profile::linux_nfsserver {
+
+  package { 'nfs-kernel-server':
+    ensure => installed,
+  }
+
+  file { '/srv/nfs_share':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'labadmins',
+    mode   => '0775',
+  }
+
+  file_line { 'nfs_export_config':
+    path   => '/etc/exports',
+    line   => '/srv/nfs_share  *(rw,sync,no_subtree_check)',
+    notify => Exec['update_nfs_exports'],
+  }
+
+  exec { 'update_nfs_exports':
+    command     => '/usr/sbin/exportfs -a',
+    refreshonly => true,
+    subscribe   => Package['nfs-kernel-server'],
+  }
+}
