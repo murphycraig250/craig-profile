039ec94f850edd0edfcb10589427fd264842767f
craig <murphycraig250@gmai.com>
2026-02-24 16:57:11 +0000

    update linux nfs profile

--- manifests/linux_nfsserver.pp
+++ manifests/linux_nfsserver.pp
@@ -3,22 +3,30 @@ class profile::linux_nfsserver {
     ensure => installed,
   }
 
+  service { 'nfs-kernel-server':
+    ensure  => running,
+    enable  => true,
+    require => Package['nfs-kernel-server'],
+  }
+
   file { '/srv/nfs_share':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'labadmins',
-    mode   => '0775',
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'labadmins',
+    mode    => '0775',
+    require => Package['nfs-kernel-server'],
   }
 
   file_line { 'nfs_export_config':
-    path   => '/etc/exports',
-    line   => '/srv/nfs_share  *(rw,sync,no_subtree_check)',
-    notify => Exec['update_nfs_exports'],
+    path    => '/etc/exports',
+    line    => '/srv/nfs_share  *(rw,sync,no_subtree_check)',
+    notify  => Exec['update_nfs_exports'],
+    require => File['/srv/nfs_share'],
   }
 
   exec { 'update_nfs_exports':
     command     => '/usr/sbin/exportfs -a',
     refreshonly => true,
-    subscribe   => Package['nfs-kernel-server'],
+    subscribe   => Service['nfs-kernel-server'],
   }
 }
