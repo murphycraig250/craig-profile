0aead94abcd67a2a52d59ee6862942591b2bcb37
craig <murphycraig250@gmai.com>
2026-02-26 19:19:05 +0000

    add puppetboard

--- manifests/linux_puppetboard.pp
+++ manifests/linux_puppetboard.pp
@@ -1,6 +1,6 @@
 class profile::linux_puppetboard {
   class { 'puppetboard':
-    python_version => '3.9',
+    python_version => '3.10',
     secret_key     => stdlib::fqdn_rand_string(32),
     puppetdb_host  => 'ubuntu-primary.localdomain', # your PuppetDB hostname/IP
   }
