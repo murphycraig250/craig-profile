154be8d9b9301f7b1875fb0a85a63c132bcc9334
craig <murphycraig250@gmai.com>
2026-02-26 19:08:17 +0000

    add puppetboard

--- manifests/linux_puppetboard.pp
+++ manifests/linux_puppetboard.pp
@@ -1,7 +1,7 @@
 class profile::linux_puppetboard {
   class { 'puppetboard':
     python_version => '3.9',
-    secret_key     => fqdn_rand_string(32),
+    secret_key     => stdlib::fqdn_rand_string(32),
     listen_address => '0.0.0.0',  # binds to all VM network interfaces
     listen_port    => 5000,
     puppetdb_host  => 'ubuntu-primary.localdomain', # your PuppetDB hostname/IP
