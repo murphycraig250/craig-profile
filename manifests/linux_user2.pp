80c84c4f254c4acab3f5c288e173af8674c41bc3
craig <murphycraig250@gmai.com>
2026-03-10 10:55:11 +0000

    add linux user 2

--- manifests/linux_user2.pp
+++ manifests/linux_user2.pp
@@ -4,26 +4,5 @@
 class profile::linux_user2 (
   Hash $users = lookup ('profile::linux_user2::users', Hash, 'deep', {}),
 ) {
-  include accounts
-
   create_resources('accounts::user', $users)
-
-  group { 'labadmins':
-    ensure => 'present',
-    gid    => 2000,
-  }
-
-  class { 'sudo':
-    config_file_replace => false,
-    purge               => true,
-    purge_ignore        => [
-      'vagrant',
-      'README',
-    ],
-  }
-
-  sudo::conf { 'labadmins':
-    priority => 10,
-    content  => '%labadmins ALL=(ALL) NOPASSWD: ALL',
-  }
 }
