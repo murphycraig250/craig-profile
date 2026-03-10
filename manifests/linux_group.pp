ad5ae988e2508bccdb9cc94f5d874d200c476301
craig <murphycraig250@gmai.com>
2026-03-10 16:04:52 +0000

    Auto commit

--- manifests/linux_group.pp
+++ manifests/linux_group.pp
@@ -4,6 +4,12 @@
 class profile::linux_group {
   include accounts
 
+  $host_group = "${facts['networking']['hostname']}_group"
+
+  group { $host_group:
+    ensure => present,
+  }
+
   class { 'sudo':
     config_file_replace => false,
     purge               => true,
