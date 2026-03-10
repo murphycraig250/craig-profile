f26abc988e529ce2ab8b2e737913e5994d2a0fed
craig <murphycraig250@gmai.com>
2026-03-10 10:42:09 +0000

    add linux user 2

--- manifests/linux_user.pp
+++ manifests/linux_user.pp
@@ -1,3 +1,7 @@
+# Manages Linux user accounts and sudo configuration for lab administrators.
+#
+# This class configures user accounts (bradley, lucas, theo), creates the labadmins
+# group, and sets up sudo privileges for lab administrators.
 class profile::linux_user {
   include accounts
   accounts::user { 'bradley':
