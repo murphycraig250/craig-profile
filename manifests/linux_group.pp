cc4d9e27bb894238d5d9f9181661476242f61913
craig <murphycraig250@gmai.com>
2026-03-10 14:37:33 +0000

    Auto commit

--- manifests/linux_group.pp
+++ manifests/linux_group.pp
@@ -3,4 +3,18 @@
 # @summary Manages Linux group accounts and settings
 class profile::linux_group {
   include accounts
+
+  class { 'sudo':
+    config_file_replace => false,
+    purge               => true,
+    purge_ignore        => [
+      'vagrant',
+      'README',
+    ],
+  }
+
+  sudo::conf { 'labadmins':
+    priority => 10,
+    content  => '%labadmins ALL=(ALL) NOPASSWD: ALL',
+  }
 }
--- manifests/linux_user2.pp
+++
@@ -1,8 +0,0 @@
-# Manages Linux user accounts using the accounts module.
-#
-# @param users A hash of user accounts to create, looked up from Hiera.
-class profile::linux_user2 (
-  Hash $users = lookup ('profile::linux_user2::users', Hash, 'deep', {}),
-) {
-  create_resources('accounts::user', $users)
-}
