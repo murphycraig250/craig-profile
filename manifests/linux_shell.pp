92a38183d3739d4d8dec71008790b85ff5b251d6
craig <murphycraig250@gmai.com>
2026-03-11 14:24:03 +0000

    Auto commit

---
+++ manifests/linux_shell.pp
@@ -0,0 +1,28 @@
+class profile::linux_shell {
+  file { '/etc/profile.d/puppet_shortcuts.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => @(EOF)
+      # Add Puppet to PATH
+      export PATH="$PATH:/opt/puppetlabs/bin"
+
+      # Function: pl <key>
+      # Usage: pl profile::linux_user::user_list
+      pl() {
+        if [ -z "$1" ]; then
+          echo "Usage: pl <lookup_key>"
+        else
+          puppet lookup "$1" --environment production --explain
+        fi
+      }
+
+      # Shortcuts
+      alias p='puppet'
+      alias pa='sudo puppet agent -t'
+      alias pcd='cd /etc/puppetlabs/code/environments/production/modules'
+      alias pd='sudo r10k deploy environment production -p -v'
+    | EOF
+  }
+}
