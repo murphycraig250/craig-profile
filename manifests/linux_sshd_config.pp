a4392ffa0377893114b00335c27b72372cd37387
Craig Murphy <murphycraig250@gmail.com>
2026-03-15 12:30:21 +0000

    sshd profile

---
+++ manifests/linux_sshd_config.pp
@@ -0,0 +1,21 @@
+# @summary Configures sshd config for linux
+# 
+# Manages the sshd_config file via template
+
+class profile::linux_sshd_config {
+  file { '/etc/ssh/sshd_config':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => template('profile/sshd_config.erb'),
+    notify  => Service['ssh'],
+  }
+
+  file { '/etc/ssh/sshd_config.d':
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    force   => true,
+  }
+}
