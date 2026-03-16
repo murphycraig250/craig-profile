82b696a5914d9178b15a7c074687f85301296197
craig <murphycraig250@gmai.com>
2026-03-16 19:37:18 +0000

    Auto commit

--- manifests/linux_firewall/rules.pp
+++ manifests/linux_firewall/rules.pp
@@ -10,21 +10,21 @@ class profile::linux_firewall::rules {
 
   # 1. Allow SSH (So you don't lock Bradley out again!)
   firewall { '100 allow ssh access':
-    dport  => 22,
-    proto  => 'tcp',
-    jump   => 'accept',
+    dport => 22,
+    proto => 'tcp',
+    jump  => 'accept',
   }
 
   # 2. Allow HTTP/HTTPS
   firewall { '101 allow http and https':
-    dport  => [80, 443],
-    proto  => 'tcp',
-    jump   => 'accept',
+    dport => [80, 443],
+    proto => 'tcp',
+    jump  => 'accept',
   }
 
   firewall { '110 allow puppet server access':
-    dport  => 8140,
-    proto  => 'tcp',
-    jump   => 'accept',
+    dport => 8140,
+    proto => 'tcp',
+    jump  => 'accept',
   }
 }
