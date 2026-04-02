477d86ec9a6c38654748005f987f908bc314de77
craig <murphycraig250@gmai.com>
2026-04-02 23:42:40 +0100

    Auto commit

--- manifests/windows_domain_join.pp
+++ manifests/windows_domain_join.pp
@@ -6,38 +6,41 @@
 #   include profile::windows_domain_join
 #
 class profile::windows_domain_join { # Inside profile::windows_domain_join
-  $dc_query = puppetdb_query("resources { type = 'Host' and title = 'dc-dns' }")
+  if $facts['networking']['domain'] != 'localdomain.test' {
+    $dc_query = puppetdb_query("resources { type = 'Host' and title = 'dc-dns' }")
 
-  if ! empty($dc_query) {
-    $dc_ip = $dc_query[0]['parameters']['ip']
+    if ! empty($dc_query) {
+      $dc_ip = $dc_query[0]['parameters']['ip']
 
-    # 1. Force the DNS change
-    exec { 'set_dns_to_dc':
-      command  => "Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses '${dc_ip}'",
-      unless   => "if ((Get-DnsClientServerAddress -InterfaceAlias 'Ethernet').ServerAddresses -contains '${dc_ip}') { exit 0 } else { exit 1 }",
-      provider => 'powershell',
-      before   => Dsc_computer['join_to_domain'],
-    }
+      # 1. Force the DNS change
+      exec { 'set_dns_to_dc':
+        command  => "Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses '${dc_ip}'",
+        unless   => "if ((Get-DnsClientServerAddress -InterfaceAlias 'Ethernet').ServerAddresses -contains '${dc_ip}') { exit 0 } else { exit 1 }",
+        provider => 'powershell',
+        before   => Dsc_computer['join_to_domain'],
+      }
 
-    # 2. Perform the Domain Join
-    dsc_computer { 'join_to_domain':
-      dsc_name       => $facts['networking']['hostname'],
-      dsc_domainname => 'localdomain.test',
-      dsc_credential => {
-        'user'     => 'Administrator',
-        'password' => Sensitive('Vagrant!23'),
-      },
-      notify         => Reboot['after_join'],
+      # 2. Perform the Domain Join
+      dsc_computer { 'join_to_domain':
+        ensure         => 'present',
+        dsc_domainname => 'localdomain.test',
+        dsc_credential => {
+          'user'     => 'Administrator',
+          'password' => Sensitive('Vagrant!23'),
+        },
+        dsc_name       => $facts['networking']['hostname'],
+        notify         => Reboot['after_join'],
+      }
+      reboot { 'after_join':
+        apply   => 'finished',
+        when    => 'refreshed',
+        message => 'Puppet is rebooting the computer to complete the domain join.',
+      }
     }
-    reboot { 'after_join':
-      apply   => 'finished',
-      when    => 'refreshed',
-      message => 'Puppet is rebooting the computer to complete the domain join.',
-    }
-  }
-  else {
-    notify { 'Waiting for DC':
-      message => 'DC IP not found in PuppetDB yet. Run Puppet on the DC first!',
+    else {
+      notify { 'Waiting for DC':
+        message => 'DC IP not found in PuppetDB yet. Run Puppet on the DC first!',
+      }
     }
   }
 }
