cd9d9f1031b9b3a3cf795f6ef60abe7097b1202f
Craig Murphy <murphycraig250@gmail.com>
2026-04-04 19:31:01 +0100

    tests

---
+++ manifests/windows_users.pp
@@ -0,0 +1,15 @@
+# @summary Creates Windows Users and Groups
+#
+# This class automates the creation of Windows users and groups, ensuring that they are properly configured with the specified attributes and permissions.
+#
+# @example
+#   include profile::windows_users
+#
+class profile::windows_users {
+  dsc_aduser { Craig:
+    ensure     => 'present',
+    userName   => 'Craig',
+    password   => 'Craig!23',
+    domainName => 'localdoman.test',
+    path       => 'CN=Users,DC=localdoman,DC=test',
+  }
