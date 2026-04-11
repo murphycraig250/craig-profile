74ed622222e636314b4b58c22bad558b0ad4ad65
craig <murphycraig250@gmai.com>
2026-04-11 14:13:00 +0100

    Auto commit

---
+++ manifests/linux_docker_dir.pp
@@ -0,0 +1,47 @@
+# Manages Docker application directory structure
+#
+# @param app_name The name of the application
+# @param deploy_user The user that will own the application files
+# @param base_dir The base directory where the app_name directory will be created
+#
+# @example
+#   profile::docker_app { 'myapp':
+#     app_name   => 'myapp',
+#     deploy_user => 'appuser',
+#     base_dir   => '/srv',
+#   }
+define profile::docker_app (
+  String $app_name,
+  String $deploy_user,
+  String $base_dir = '/srv',
+) {
+  $app_path = "${base_dir}/${app_name}"
+
+  file { $app_path:
+    ensure => directory,
+    owner  => $deploy_user,
+    group  => 'docker',
+    mode   => '0755',
+  }
+
+  file { "${app_path}/data":
+    ensure => directory,
+    owner  => $deploy_user,
+    group  => 'docker',
+    mode   => '0775',
+  }
+
+  file { "${app_path}/config":
+    ensure => directory,
+    owner  => $deploy_user,
+    group  => 'docker',
+    mode   => '0775',
+  }
+
+  file { "${app_path}/compose.yaml":
+    ensure => file,
+    owner  => $deploy_user,
+    group  => 'docker',
+    mode   => '0644',
+  }
+}
---
+++ manifests/linux_docker_whoami.pp
@@ -0,0 +1,21 @@
+# Manages Docker whoami service deployment
+#
+# This profile configures and deploys the whoami Docker application
+# with appropriate compose configuration and file permissions.
+#
+class profile::linux_docker_whoami {
+  include profile::linux_docker_dir
+
+  profile::docker_app { 'whoami':
+    app_name    => 'whoami',
+    deploy_user => 'craig',
+  }
+
+  file { '/srv/whoami/compose.yaml':
+    ensure => file,
+    owner  => 'craig',
+    group  => 'docker',
+    mode   => '0644',
+    source => 'puppet:///modules/profile/docker/whoami-compose.yaml',
+  }
+}
