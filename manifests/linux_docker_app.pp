e194cf44a3ff0b021d59cf599ad935d59a9479a3
craig <murphycraig250@gmai.com>
2026-04-15 21:17:53 +0100

    Auto commit

--- manifests/linux_docker_app.pp
+++ manifests/linux_docker_app.pp
@@ -25,6 +25,7 @@ define profile::linux_docker_app (
   Optional[Variant[String, Sensitive[String]]] $docker_user     = undef,
   Optional[Variant[String, Sensitive[String]]] $docker_password = undef,
   Optional[String] $up_args = '',
+  Optional[String] $depends_on = '',
 ) {
   $app_name     = $title
   $app_path     = "${base_dir}/${app_name}"
@@ -64,18 +65,42 @@ define profile::linux_docker_app (
     group   => $deploy_group,
     mode    => '0600',
     content => epp("profile/docker/${app_name}-docker-compose.epp", {
-        'docker_user'     => $docker_user =~ Sensitive ? { true => $docker_user.unwrap,     default => $docker_user },
-        'docker_password' => $docker_password =~ Sensitive ? { true => $docker_password.unwrap, default => $docker_password },
+        'docker_user'     => $docker_user =~ Sensitive ? { true => $docker_user.'unwrap',     default => $docker_user },
+        'docker_password' => $docker_password =~ Sensitive ? { true => $docker_password.'unwrap', default => $docker_password },
     }),
+    require => File[$app_path],
   }
 
   # ----------------------------
   # 3. Container lifecycle
   # ----------------------------
+  $base_requirements = [
+    File[$compose_file],
+    File["${app_path}/data"],
+    File["${app_path}/config"]
+  ]
+
+  $docker_deps = $depends_on ? {
+    ''      => $base_requirements,
+    default => $base_requirements + [Docker_compose[$depends_on]],
+  }
+
   docker_compose { $app_name:
     ensure        => present,
     compose_files => [$compose_file],
     up_args       => $up_args,
     subscribe     => File[$compose_file],
+    require       => $docker_deps,
+  }
+
+  # ----------------------------
+  # 4. Force recreate if unhealthy
+  # ----------------------------
+  exec { "force_recreate_${app_name}_if_unhealthy":
+    path    => ['/usr/bin', '/usr/local/bin'],
+    command => "docker-compose up -d --force-recreate ${app_name}",
+    cwd     => $app_path,
+    onlyif  => "docker inspect --format='{{.State.Health.Status}}' ${app_name} | grep unhealthy",
+    require => Docker_compose[$app_name],
   }
 }
