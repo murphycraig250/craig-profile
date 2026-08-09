b0f79dcee40fa5f8aa1648b44fafe73f90921445
craig <murphycraig250@gmai.com>
2026-08-09 17:20:43 +0100

    Auto commit

--- manifests/domain_controller.pp
+++ manifests/domain_controller.pp
@@ -1,12 +1,30 @@
 class profile::domain_controller (
   String $domain_name = 'localdomain.test',
   String $domain_netbios_name = 'LOCALDOMAIN',
+
+  String $interface_alias = 'Ethernet',
+  String $ip_address = '192.168.1.60',
+  Integer $prefix_length = 24,
+  String $gateway = '192.168.1.1',
+  String $dns_server = '192.168.1.60',
+
 ) {
   file { 'C:\ProgramData\PuppetLabs\promote-dc.ps1':
     ensure  => file,
     content => epp('profile/domain_controller/promote-dc.epp', {
         'domain_name'         => $domain_name,
         'domain_netbios_name' => $domain_netbios_name,
+    }),
+  }
+
+  file { 'C:\ProgramData\PuppetLabs\configure-network.ps1':
+    ensure  => file,
+    content => epp('profile/domain_controller/configure-network.epp', {
+        'interface_alias' => $interface_alias,
+        'ip_address'      => $ip_address,
+        'prefix_length'   => $prefix_length,
+        'gateway'         => $gateway,
+        'dns_server'      => $dns_server,
     }),
   }
 }
