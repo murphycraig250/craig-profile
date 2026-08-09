67aed5f08167a1f45a3cd39b516436df87388466
craig <murphycraig250@gmai.com>
2026-08-09 17:57:54 +0100

    Auto commit

--- manifests/dc_promotion.pp
+++ manifests/dc_promotion.pp
@@ -1,5 +1,15 @@
-class profile::dc_promotion {
-  notify { 'Promoting this server to a domain controller':
-    message => 'Promoting this server to a domain controller',
+class profile::dc_promotion (
+  String $domain_name = 'localdomain.test',
+  String $domain_netbios_name = 'LOCALDOMAIN',
+  String $safe_mode_password = 'YourTemporaryPassword123!',
+) {
+
+  file { 'C:\ProgramData\PuppetLabs\promote-dc.ps1':
+    ensure  => file,
+    content => epp('profile/domain_controller/promote-dc.ps1.epp', {
+      'domain_name'        => $domain_name,
+      'domain_netbios_name' => $domain_netbios_name,
+      'safe_mode_password' => $safe_mode_password,
+    }),
   }
 }
