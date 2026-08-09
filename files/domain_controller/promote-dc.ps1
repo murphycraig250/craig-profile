e83a7ca5b91d6400c4bda7457304190c509bd286
craig <murphycraig250@gmai.com>
2026-08-09 16:37:29 +0100

    Auto commit

--- files/domain_controller/promote-dc.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-Write-Output "Puppet DC promotion script reached."
-Write-Output "PowerShell version: $($PSVersionTable.PSVersion)"
--- manifests/domain_controller.pp
+++ manifests/domain_controller.pp
@@ -1,6 +1,12 @@
-class profile::domain_controller {
+class profile::domain_controller (
+  String $domain_name = 'localdomain.test',
+  String $domain_netbios_name = 'LOCALDOMAIN',
+) {
   file { 'C:\ProgramData\PuppetLabs\promote-dc.ps1':
-    ensure => file,
-    source => 'puppet:///modules/profile/domain_controller/promote-dc.ps1',
+    ensure  => file,
+    content => epp('profile/domain_controller/promote-dc.ps1.epp', {
+        'domain_name'         => $domain_name,
+        'domain_netbios_name' => $domain_netbios_name,
+    }),
   }
 }
