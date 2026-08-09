db37ad50531bcb4c9402d53227a8920c21013450
craig <murphycraig250@gmai.com>
2026-08-09 17:28:03 +0100

    Auto commit

--- manifests/domain_controller.pp
+++ manifests/domain_controller.pp
@@ -19,12 +19,18 @@ class profile::domain_controller (
 
   file { 'C:\ProgramData\PuppetLabs\configure-network.ps1':
     ensure  => file,
-    content => epp('profile/domain_controller/configure-network.epp', {
+    content => epp('profile/domain_controller/configure-network.ps1.epp', {
         'interface_alias' => $interface_alias,
         'ip_address'      => $ip_address,
         'prefix_length'   => $prefix_length,
         'gateway'         => $gateway,
         'dns_server'      => $dns_server,
     }),
+  }
+
+  exec { 'configure-domain-controller-network':
+    command     => 'pwsh.exe -NoProfile -File C:\ProgramData\PuppetLabs\configure-network.ps1',
+    refreshonly => true,
+    subscribe   => File['C:\ProgramData\PuppetLabs\configure-network.ps1'],
   }
 }
