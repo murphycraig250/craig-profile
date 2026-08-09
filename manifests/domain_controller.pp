6e2d530675308017debe5b7d956f71df6cb6b710
craig <murphycraig250@gmai.com>
2026-08-09 16:34:59 +0100

    Auto commit

--- manifests/domain_controller.pp
+++ manifests/domain_controller.pp
@@ -1,6 +1,6 @@
 class profile::domain_controller {
   file { 'C:\ProgramData\PuppetLabs\promote-dc.ps1':
     ensure => file,
-    source => 'puppet:///modules/craig-profile/promote-dc.ps1',
+    source => 'puppet:///modules/profile/domain_controller/promote-dc.ps1',
   }
 }
