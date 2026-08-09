d82c684a649d6bc8432bfbc9270e54cc28639deb
craig <murphycraig250@gmai.com>
2026-08-09 22:56:39 +0100

    Auto commit

--- manifests/windows_domain_join.pp
+++ manifests/windows_domain_join.pp
@@ -1,13 +1,15 @@
-exec { 'join-domain':
-  command  => "${cred} = New-Object System.Management.Automation.PSCredential(''DOMAIN\vagrant'', (ConvertTo-SecureString ''vagrant'' -AsPlainText -Force)); Add-Computer -DomainName ''localdomain.test'' -OUPath ''OU=Lab Computers,DC=localdomain,DC=test'' -Credential ${cred} -Force'",
-  provider => powershell,
-  unless   => 'try { if ((Get-CimInstance Win32_ComputerSystem).PartOfDomain) { exit 0 } catch { exit 1 }',
-  timeout  => 300,
-  notify   => Exec['reboot-after-domain-join'],
-}
+class profile::windows_domain_join {
+  exec { 'join-domain':
+    command  => "${cred} = New-Object System.Management.Automation.PSCredential(''DOMAIN\vagrant'', (ConvertTo-SecureString ''vagrant'' -AsPlainText -Force)); Add-Computer -DomainName ''localdomain.test'' -OUPath ''OU=Lab Computers,DC=localdomain,DC=test'' -Credential ${cred} -Force'",
+    provider => powershell,
+    unless   => 'try { if ((Get-CimInstance Win32_ComputerSystem).PartOfDomain) { exit 0 } catch { exit 1 }',
+    timeout  => 300,
+    notify   => Exec['reboot-after-domain-join'],
+  }
 
-exec { 'reboot-after-domain-join':
-  command     => 'shutdown.exe /r /t 5 /c "Rebooting after domain join"',
-  refreshonly => true,
-  path        => ['C:/Windows/System32'],
+  exec { 'reboot-after-domain-join':
+    command     => 'shutdown.exe /r /t 5 /c "Rebooting after domain join"',
+    refreshonly => true,
+    path        => ['C:/Windows/System32'],
+  }
 }
