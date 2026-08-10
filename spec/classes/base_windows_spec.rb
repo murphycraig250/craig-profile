e905fc4cfeff0f57e7885d0c134b16e58680f539
craig <murphycraig250@gmai.com>
2026-08-10 21:10:04 +0100

    Auto commit

--- spec/classes/base_windows_spec.rb
+++ spec/classes/base_windows_spec.rb
@@ -4,6 +4,10 @@ require 'spec_helper'
 
 describe 'profile::base_windows' do
   on_supported_os.each do |os, os_facts|
+    # Skip Windows catalog compilation on non-Windows test hosts (like Linux CI)
+    # because the windows_adsi provider for the group resource requires Windows APIs.
+    next if os_facts[:os]['family'] == 'windows' && !Gem.win_platform?
+
     context "on #{os}" do
       let(:facts) { os_facts }
 
