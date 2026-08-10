8bae9750c4b1420317966fba136727fd8a25f6b7
craig <murphycraig250@gmai.com>
2026-08-10 22:15:31 +0100

    Auto commit

--- manifests/windows_domain_join.pp
+++ manifests/windows_domain_join.pp
@@ -1,39 +1,63 @@
 class profile::windows_domain_join {
-
-  # AD/DNS server
   $domain_dns = '192.168.1.60'
   $domain     = 'localdomain.test'
   $domain_ou = 'OU=Lab Computers,DC=localdomain,DC=test'
 
-  # Make sure the Windows machine uses the AD DNS server.
+  # --------------------------------------------------------------------------
+  # Configure DNS to use the domain controller
+  # --------------------------------------------------------------------------
+
   exec { 'configure-domain-dns':
     command  => "Get-NetAdapter | Where-Object { \$_.Status -eq 'Up' } | ForEach-Object { Set-DnsClientServerAddress -InterfaceIndex \$_.ifIndex -ServerAddresses '${domain_dns}' }",
     provider => powershell,
     unless   => "Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { \$_.ServerAddresses -contains '${domain_dns}' }",
   }
 
-  # Wait until the AD DNS server is reachable before attempting the join.
+  # --------------------------------------------------------------------------
+  # Verify that AD DNS is actually responding
+  # --------------------------------------------------------------------------
+
   exec { 'verify-domain-dns':
-    command  => "if (-not (Resolve-DnsName -Name '_ldap._tcp.dc._msdcs.${domain}' -Server '${domain_dns}' -ErrorAction SilentlyContinue)) { exit 1 }",
+    command  => "Resolve-DnsName -Name '_ldap._tcp.dc._msdcs.${domain}' -Server '${domain_dns}' -ErrorAction Stop",
     provider => powershell,
     require  => Exec['configure-domain-dns'],
     timeout  => 60,
   }
 
-  # Join the machine to the domain.
+  # --------------------------------------------------------------------------
+  # Join the domain
+  # --------------------------------------------------------------------------
+
   exec { 'join-domain':
-    command  => "\$cred = New-Object System.Management.Automation.PSCredential(\"DOMAIN\\vagrant\", (ConvertTo-SecureString \"vagrant\" -AsPlainText -Force)); Add-Computer -DomainName \"${domain}\" -OUPath \"${domain_ou}\" -Credential \$cred -Force",
+    command  => "\$cred = New-Object System.Management.Automation.PSCredential(\"DOMAIN\\vagrant\", (ConvertTo-SecureString \"vagrant\" -AsPlainText -Force)); Add-Computer -DomainName \"${domain}\" -OUPath \"${domain_ou}\" -Credential \$cred -Force -ErrorAction Stop",
     provider => powershell,
     unless   => 'if ((Get-CimInstance Win32_ComputerSystem).PartOfDomain) { exit 0 } else { exit 1 }',
     require  => Exec['verify-domain-dns'],
     timeout  => 300,
-    notify   => Exec['reboot-after-domain-join'],
   }
 
-  # Reboot only when Puppet has actually changed the domain membership.
+  # --------------------------------------------------------------------------
+  # Verify domain membership
+  #
+  # This is deliberately separate from the join command.
+  # If the join fails, Puppet will NOT reboot the machine.
+  # --------------------------------------------------------------------------
+
+  exec { 'verify-domain-join':
+    command  => 'if ((Get-CimInstance Win32_ComputerSystem).PartOfDomain) { exit 0 } else { Write-Error "Computer is NOT joined to a domain"; exit 1 }',
+    provider => powershell,
+    require  => Exec['join-domain'],
+  }
+
+  # --------------------------------------------------------------------------
+  # Reboot ONLY after successful domain membership verification
+  # --------------------------------------------------------------------------
+
   exec { 'reboot-after-domain-join':
-    command     => 'shutdown.exe /r /t 5 /c "Rebooting after domain join"',
-    refreshonly => true,
+    command     => 'shutdown.exe /r /t 10 /c "Rebooting after successful domain join"',
+    provider    => shell,
     path        => ['C:/Windows/System32'],
+    refreshonly => true,
+    subscribe   => Exec['verify-domain-join'],
   }
 }
